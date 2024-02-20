34062c8c73243879db627ef51f7d8b9e8c53f35e
Christopher Thai <christopherthai.nyli@gmail.com>
2024-02-19 22:52:10 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array) {
+  const newArray = array.slice(0, 2);
+  return newArray;
+}
+
+const returnLastTwoDrivers = function(array) {
+  const newArray = array.slice(-2);
+  return newArray;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(fareMultiplier) {
+  return function(fare){
+    return fare * fareMultiplier;
+  }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(arrayOfDrivers, returnDriversFunction) {
+  return returnDriversFunction(arrayOfDrivers);
+}
